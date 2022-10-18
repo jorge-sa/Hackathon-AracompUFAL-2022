e0b124ad5619d23d13e984932b151c86be0a9b00
Moraniel <moraniel@outlook.com>
2022-10-18 16:47:31 -0300

    Troca de API

--- recicle_bin/src/App.js
+++ recicle_bin/src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Maps from './components/Maps.js';
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from "leaflet";
+
+
 
 function App() {
 
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Teste</h1>
-      </header>
-    </div>
-  );
+    <main>
+      <MapContainer ClassName="leaflet-container" center={[-9.701426, -36.688151]} zoom={20} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+    </main>
+
+
+
+  )
 }
 
 export default App;
