e420ddde09955da3a23547e7fcc6c29c110de3d6
Moraniel <moraniel@outlook.com>
2022-10-18 13:28:54 -0300

    Iniciando com React

---
+++ recicle_bin/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Teste</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
