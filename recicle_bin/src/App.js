87609f53276483399fc84c19765a9543c2c408a3
Moraniel <moraniel@outlook.com>
2022-10-19 11:08:06 -0300

    Melhoria no CSS

--- recicle_bin/src/App.js
+++ recicle_bin/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import recicleBin from "./data/dados.json"
+import recicleBin from "./data/recicle-bin.json"
 import materials from "./data/materials.json"
 import { geoJSON, icon } from 'leaflet';
 import L from "leaflet"
@@ -21,8 +21,9 @@ function App() {
  {materials.find(mat=>(selects=== mat.name))}
 
   return (
-  <main class = "bg-green-500 grid justify-items-stretch space-y-5"  >
-    
+  <main>
+      <body class = "rgba(32, 126, 20, 0.493) space-y-5" >
+      
     <h1 class = 'font-white font-normal text-white text-center text-6xl '>RecicleFind </h1>
     <h1 class = 'font-white font-normal text-center ju  text-6xl '>Selecione o material </h1>
     
@@ -36,8 +37,8 @@ function App() {
     <option value="Teclado">Teclado</option>
     </select>
     
-    <h1 class='text-center  text-white italic font-bold text-4xl '>Tipo de Lixeira para o material:</h1>     
-    <h1 id = "type" class='font-normal  justify-self-center text-3xl h-16 w-50'>    {materials.map((mat,index)=> {
+    <h1 class='text-center  text-white italic font-bold text-4xl h-13 w-50 rounded'>Tipo de Lixeira para o material:</h1>     
+    <h1 id = "type" class='font-normal  justify-self-center text-3xl '>    {materials.map((mat,index)=> {
       if (mat.name === selects) {
         return <div id ="mat">
         <h1>{mat.type}</h1>
@@ -49,12 +50,13 @@ function App() {
     <h1 id="mapTitle" class='justify-self-center text-5xl italic font-bold'> Locais com Coleta Seletiva</h1>
     
     
-    <MapContainer class='leaflet-container'  center={[-9.701426, -36.688151]} zoom={30} scrollWheelZoom={false}>
+    <MapContainer class='leaflet-container'  center={[-9.701426, -36.688151]} zoom={45} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-{recicleBin.map(bin=>( 
+{
+recicleBin.map(bin=>( 
         <Marker 
         key = {bin.id}
         position={[bin.gps.latitude, bin.gps.longitude]}
@@ -65,6 +67,8 @@ function App() {
       
 
     </MapContainer>
+    </body>
+  
   </main>
 
   )
